d89562f0f331ec0fbb4652ebca65c77a2a0cd6cd
ravengerUA <yurisikorsky@gmail.com>
2015-11-30 15:53:35 +0200

    fixed updating

--- src/views/BookcaseView.js
+++ src/views/BookcaseView.js
@@ -20,49 +20,28 @@ export class BookcaseView extends Component {
   render () {
     let bookcaseName;
 
-    console.log(this.props);
-    return (
-      <div className='container'>
+    return <div className='container'>
         <h1>Книжные полки</h1>
         <input ref={node => { bookcaseName = node; }} />
         <button className="btn" onClick={() => {
           this.props.actions.add_bookcase({
             bookcase_id: 100,
             name: bookcaseName.value,
-            type: 'work'
+            group: 'work'
           })
         }}>
           Добавить полку
         </button>
 
-
         {this.props.bookcases.some(bookcase => bookcase.group === 'work')
           ? <BookcaseList bookcases={this.props.bookcases} group='work' onDeleteClick={this.props.actions.remove_bookcase} />
           : null
         }
-        {this.props.bookcases.some(bookcase => bookcase.group === 'work')
+        {this.props.bookcases.some(bookcase => bookcase.group === 'edition')
           ? <BookcaseList bookcases={this.props.bookcases} group='edition' onDeleteClick={this.props.actions.remove_bookcase} />
           : null
         }
-
-        <h2>Другие полки</h2>
-        <ul>
-        {this.props.bookcases.sort((a, b) => a.order > b.order).map(bookcase =>
-          <li key={bookcase.bookcase_id} className={`bookcase_item ${bookcase.type}`}>
-            <Link to={`/bookcases/${bookcase.bookcase_id}`}>{bookcase.name}</Link>
-            <span className="description"> ({bookcase.description})</span>
-            <span className="order"> (#{bookcase.order})</span>
-            <span className="bookcount"> (книг: {bookcase.bookcount})</span>
-            <div>
-              <button className="btn btn-sm" onClick={() => {
-                this.props.actions.remove_bookcase(bookcase.bookcase_id);
-              }}>Удалить полку</button>
-            </div>
-          </li>
-        )}
-        </ul>
       </div>
-    );
   }
 }
 
